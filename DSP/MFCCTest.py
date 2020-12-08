10e3f7a219badb21b4bfe9ef46566aa7327cdad2
Christopher Jung <cjung@bu.edu>
2020-12-08 00:13:26 -0500

    changes to MFCCTEST file with flask app and aws

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -2,11 +2,21 @@
 from MFCC import mfcc
 from MFCC import delta
 from MFCC import logfbank
+from flask import Flask
 import scipy.io.wavfile as wav
 
-(rate,sig) = wav.read("../sounds/english.wav")
+app = Flask(__name__)
+
+(rate,sig) = wav.read("english.wav")
 mfcc_feat = mfcc(sig, rate)
 d_mfcc_feat = delta(mfcc_feat, 2)
 fbank_feat = logfbank(sig, rate)
 
-print(fbank_feat[1:3,:])
+@app.route('/')
+def dsp():
+    return fbank_feat[1:3,:]
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
+#print(fbank_feat[1:3,:])

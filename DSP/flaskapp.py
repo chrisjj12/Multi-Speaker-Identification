47776ba84697fea6dfbfbbd3874c1dcd21fc2245
Christopher Jung <cjung@bu.edu>
2020-12-07 23:49:17 -0500

    changes

--- DSP/flaskapp.py
+++ DSP/flaskapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/dsp')
 def dsp():
-    return "DSP"
+    return 'DSP'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)

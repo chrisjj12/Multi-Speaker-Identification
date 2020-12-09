b494f2bd7fcec94e4728830c2c1bbdcb2d8bc0f5
Christopher Jung <cjung@bu.edu>
2020-12-08 22:09:06 -0500

    kcoeff successfully printing on flask app

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -10,11 +10,11 @@ import scipy.io.wavfile as wav
 
 app = Flask(__name__, template_folder="Templates")
 FlaskJSON(app)
-#file_name = "kcoeff.json"
+
 
 @app.route('/')
 
-    #return render_template('main.html')
+
 
 
 
@@ -28,53 +28,10 @@ def create_json():
     database_format = json.dumps({"Name": json_conv}) # Need to change to the user inputed name in the application
 
 
-#print(json.dumps({"Name": json_conv})) # Need to change to the user inputed name in the application
 
-#print(database_format)
 
     return render_template('main.html', dblist =  database_format)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
-
-#print(fbank_feat[1:3,:])w
-
-#course_list = list(client.db.course_col.find({"major": major.upper()}))
- #   return flask.jsonify(**course_list)
-
-"""
-from datetime import datetime
-from flask import Flask, request
-from flask_json import FlaskJSON, JsonError, json_response, as_json
-
-app = Flask(__name__)
-FlaskJSON(app)
-
-
-@app.route('/get_time')
-def get_time():
-    now = datetime.utcnow()
-    return json_response(time=now)
-
-
-@app.route('/increment_value', methods=['POST'])
-def increment_value():
-    # We use 'force' to skip mimetype checking to have shorter curl command.
-    data = request.get_json(force=True)
-    try:
-        value = int(data['value'])
-    except (KeyError, TypeError, ValueError):
-        raise JsonError(description='Invalid value.')
-    return json_response(value=value + 1)
-
-
-@app.route('/get_value')
-@as_json
-def get_value():
-    return dict(value=12)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
-"""

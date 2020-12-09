68ca177fccc9f9dae88476539649d9802fb5021e
Christopher Jung <cjung@bu.edu>
2020-12-08 22:09:25 -0500

    attempting to create a txt file from the flask app

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -7,16 +7,14 @@ from flask_json import FlaskJSON, JsonError, json_response, as_json
 import json
 import scipy.io.wavfile as wav
 
+f= open("kcoeff.txt","w+")
+
+
 
 app = Flask(__name__, template_folder="Templates")
 FlaskJSON(app)
 
-
 @app.route('/')
-
-
-
-
 
 def create_json():
     (rate,sig) = wav.read("english.wav")
@@ -26,12 +24,16 @@ def create_json():
     python_arr = fbank_feat[1:3,:]
     json_conv = python_arr.tolist()
     database_format = json.dumps({"Name": json_conv}) # Need to change to the user inputed name in the application
-
-
+    f= open("kcoeff.txt","a+")
+    for i in range(2):
+        f.write(database_format[i])
 
 
     return render_template('main.html', dblist =  database_format)
+    f.close()
 
+f.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
+

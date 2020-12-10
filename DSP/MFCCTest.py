f8ecef18b06b19409608f07f84b3f7c933cbd581
Christopher Jung <cjung@bu.edu>
2020-12-10 03:17:26 -0500

    changing wave file

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -2,37 +2,54 @@
 from MFCC import mfcc
 from MFCC import delta
 from MFCC import logfbank
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, flash, render_template, request, send_file, jsonify, redirect, url_for
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 import json
 import scipy.io.wavfile as wav
 import os
 from pydub import AudioSegment
 
+#UPLOAD_FOLDER = '/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'
+#ALLOWED_EXTENSIONS = {'m4a', 'wav'}
 
-
-app = Flask(__name__, template_folder="Templates")
+app = Flask(__name__, template_folder = "Templates")
 FlaskJSON(app)
-
+#app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'] = UPLOAD_FOLDER
+"""
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+"""
 @app.route('/')
 
+#def upload_file():
+    if request.method == 'POST':
+        file = request.files['hello.m4a']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'], filename))
+
 def create_json():
-    """
-    src = open(os.path.join("/Users/chrisjung/Downloads", "hello.m4a"))
-    dst = "test.wav"
+    
+    file = request.files['hello.m4a']
+        if file:
+            filename = secure_filename(file.filename)
+            src = file.save(os.path.join(app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'], filename))
+    #src = open(os.path.join("/Users/chrisjung/Downloads", "hello.m4a"))
+            dst = "test.wav"
 
 
-    # convert wav to mp3                                                            
-    sound = AudioSegment.from_file(src, "aac")
-    wavfile = sound.export(dst, format="wav")
-    """
-    (rate,sig) = wav.read("hello.wav")
-    mfcc_feat = mfcc(sig, rate)
-    d_mfcc_feat = delta(mfcc_feat, 2)
-    fbank_feat = logfbank(sig, rate) 
-    python_arr = fbank_feat[1:3,:]
-    json_conv = python_arr.tolist()
-    database_format = json.dumps({"Chris": json_conv}) # Need to change to the user inputed name in the application
+            # convert wav to mp3                                                            
+            sound = AudioSegment.from_file(src, "aac")
+            wavfile = sound.export(dst, format="wav")
+            
+            (rate,sig) = wav.read(wavfile)
+            mfcc_feat = mfcc(sig, rate)
+            d_mfcc_feat = delta(mfcc_feat, 2)
+            fbank_feat = logfbank(sig, rate) 
+            python_arr = fbank_feat[1:3,:]
+            json_conv = python_arr.tolist()
+            database_format = json.dumps({"Chris": json_conv}) # Need to change to the user inputed name in the application
 
 
     return render_template('main.html', dblist =  database_format)

0223239e2ed864ddcaffaf1767a6a6f821104047
Christopher Jung <cjung@bu.edu>
2020-12-10 04:24:12 -0500

    changing wave file

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -29,12 +29,14 @@ def create_json():
 
 
             # convert wav to mp3                                                            
-    sound = AudioSegment.from_file("hello.m4a", format = "m4a")
-    wavfile = sound.export("test.wav", format="wav")
-    file_name = wavefile.name
+    #sound = AudioSegment.from_file("hello.m4a", format = "m4a")
+    
+    #wavfile = sound.export("test.wav", format="wav")
+    
+    #file_name = wavefile.name
    
             
-    (rate,sig) = wav.read(file_name)
+    (rate,sig) = wav.read("test.wav")
     mfcc_feat = mfcc(sig, rate)
     d_mfcc_feat = delta(mfcc_feat, 2)
     fbank_feat = logfbank(sig, rate) 

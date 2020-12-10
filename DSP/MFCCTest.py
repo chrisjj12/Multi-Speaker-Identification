26a066447c07fbbbce073a3831737acc0c87c5f4
Christopher Jung <cjung@bu.edu>
2020-12-10 03:57:53 -0500

    changing wave file

--- DSP/MFCCTest.py
+++ DSP/MFCCTest.py
@@ -9,35 +9,15 @@ import scipy.io.wavfile as wav
 import os
 from pydub import AudioSegment
 
-#UPLOAD_FOLDER = '/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'
-#ALLOWED_EXTENSIONS = {'m4a', 'wav'}
 
 app = Flask(__name__, template_folder = "Templates")
 FlaskJSON(app)
-#app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'] = UPLOAD_FOLDER
-"""
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-"""
+
 @app.route('/')
 
 
 def create_json():
-    """
-    if request.method == 'POST':
-        file = request.files['hello.m4a']
-        if file:
-            filename = secure_filename(file.filename)
-            src = file.save(os.path.join(app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'], filename))
-    #src = open(os.path.join("/Users/chrisjung/Downloads", "hello.m4a"))
-            dst = "test.wav"
 
-
-            # convert wav to mp3                                                            
-            sound = AudioSegment.from_file(src, "m4a")
-            wavfile = sound.export(dst, format="wav")
-"""          
     (rate,sig) = wav.read("hello.m4a")
     mfcc_feat = mfcc(sig, rate)
     d_mfcc_feat = delta(mfcc_feat, 2)

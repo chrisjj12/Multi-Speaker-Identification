690b0fccd76f39013883fe4432f4744c9c7045e8
Christopher Jung <cjung@bu.edu>
2020-12-11 09:40:34 -0500

    latest changes

--- MSI/App.js
+++ MSI/App.js
@@ -2,10 +2,13 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { Component } from 'react';
-import {Text, View,} from 'react-native';
+import {Platform, Text, View,} from 'react-native';
 import AudioRecorderPlayer, {AVEncoderAudioQualityIOSType, AVEncodingOption, AudioSet, AudioEncoderAndroidType, AudioSourceAndroidType} 
 from 'react-native-audio-recorder-player';
 import { Card, Divider, Button, Title } from 'react-native-paper';
+import { RNS3 } from 'react-native-aws3';
+
+
 //import com.rnfs.RNFSPackage; 
 
 //import Voice from 'react-native-voice';
@@ -20,6 +23,7 @@ export default class App extends Component {
         <Tab.Navigator initialRouteName = "Home">
           <Tab.Screen name = "Home" component = {homepage}/>
           <Tab.Screen name = "First Time User" component = {first_time_user}/>
+          <Tab.Screen name = "Speaker Identifier" component = {speak_ident}/>
         </Tab.Navigator>
       </NavigationContainer>
     );    
@@ -30,7 +34,7 @@ function homepage () {
   return(
     <View style = {{flex: 1, justifyContent: "center", alignItems: "center"}}>
       <Text style = {{textAlignVertical: "bottom", textAlign: "center", fontSize: 30, color: "black"}}>
-        Multi-Speaker Indentification
+        Speaker Recognition
       </Text>
     </View>
   );
@@ -46,10 +50,11 @@ class first_time_user extends React.Component {
       currentPostiionSec: 0,
       currentDurationSec: 0,
       playTime: '00:00:00',
-      duration: '00:00:00'
+      duration: '00:00:00',
     };
     this.audioRecorderPlayer = new AudioRecorderPlayer();
     this.audioRecorderPlayer.setSubscriptionDuration(0.09); //default is 0.1
+    this.stop_recording = this.stop_recording.bind(this);
   }
 
   start_recording = async() => {
@@ -63,8 +68,7 @@ class first_time_user extends React.Component {
       ios: 'hello.wav' //wav
     }) 
     */
-    const path = 'hello.m4a'; //change this file name to the name inputted
-    //const uri = await audioRecorderPlayer.startRecord(path);
+    const path = 'Chris.m4a'; //change this file name to the name inputted
     const AudioSet = {
       AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
       AudioSourceAndroid: AudioSourceAndroidType.MIC,
@@ -73,7 +77,7 @@ class first_time_user extends React.Component {
       AVFormatIDIOS: AVEncodingOption.aac,
     };
     console.log('AudioSet', AudioSet);
-    const uri = await this.audioRecorderPlayer.startRecorder(path, AudioSet)
+    const URI = await this.audioRecorderPlayer.startRecorder(path, AudioSet)
     this.audioRecorderPlayer.addRecordBackListener((e) => {
       this.setState({
         recordSecs: e.current_position,
@@ -82,16 +86,44 @@ class first_time_user extends React.Component {
         ),
       });
     });
-    console.log(`uri: ${uri}`);
-    return uri
+    console.log(`uri: ${URI}`);
   };
 
   stop_recording = async() => {
     const result  = await this.audioRecorderPlayer.stopRecorder();
+    
+
+    const file = {
+      uri: result,
+      name: result,
+      type: `audio/aac`
+    };
+
+    const options = {
+      keyPrefix: 'audio/',
+      bucket: 'iostoflask',
+      region: 'us-east-2',
+      accessKey: 'AKIAJ6QLTOKIBPZBR65A',
+      secretKey: '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',
+    };
+
+    RNS3.put(file, options)
+      .progress(event => {
+        console.log(`percent: ${event.percent}`);
+      })
+      .then(response => {
+        console.log(response, "response from rns3 audio");
+        if (response.status !== 201) {
+          console.error(response.body);
+          return;
+        }
+      })
+
     this.audioRecorderPlayer.removeRecordBackListener();
     this.setState({
       recordSecs: 0,
     });
+
     
     console.log(result);
   };
@@ -104,7 +136,7 @@ class first_time_user extends React.Component {
       android: 'sdcard/first_time.wav', // should give extra dir name in android. Won't grant permission to the first level of dir.
     });
     */
-    const path = 'hello.m4a';
+    const path = 'Chris.m4a';
     const msg = await this.audioRecorderPlayer.startPlayer(path);
     this.audioRecorderPlayer.setVolume(1.0);
     console.log(msg);
@@ -129,26 +161,17 @@ class first_time_user extends React.Component {
     this.audioRecorderPlayer.stopPlayer();
     this.audioRecorderPlayer.removePlayBackListener();
   };
-
-  uploadFile(uri) {
-    var form = new FormData();
-    form.append('file',file)
-    fetch('http://0.0.0.0:80/', {
-        // content-type header should not be specified!
-        method: 'POST',
-        body: form
-    }).then(function (response){
-        return (response.text())
-    }).then(function(text){
-        console.log(text) // The text the endpoint returns
-    })
-        .catch(error => console.log(error)
-        );
-}
+   //DOWNLOAD COEFF.json
 
   render() {
     return (
       <Card style = {{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center'}}>
+        <Text style = {{flex: .22, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center', fontSize: 20}}>
+          Please record the displayed sentence
+        </Text>
+        <Text style = {{flex: .2, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center', fontSize: 30}}>
+          The sun is shining bright today
+        </Text>
         <Title style = {{Title: 'centered'}}>
           {this.state.recordTime}
         </Title>
@@ -174,6 +197,172 @@ class first_time_user extends React.Component {
     )
   }
 }
+
+class speak_ident extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggingIn: false,
+      recordSecs: 0,
+      recordTime: '00:00:00',
+      currentPostiionSec: 0,
+      currentDurationSec: 0,
+      playTime: '00:00:00',
+      duration: '00:00:00',
+    };
+    this.audioRecorderPlayer = new AudioRecorderPlayer();
+    this.audioRecorderPlayer.setSubscriptionDuration(0.09); //default is 0.1
+    this.stop_recording = this.stop_recording.bind(this);
+  }
+
+  start_recording = async() => {
+    /*
+    const path = Platform.select({
+      ios: 'first_time.wav',
+      android: 'sdcard/first_time.wav', // should give extra dir name in android. Won't grant permission to the first level of dir.
+    });*/
+    /*
+    const path = Platform.select({
+      ios: 'hello.wav' //wav
+    }) 
+    */
+    const path = 'Chris.m4a'; //change this file name to the name inputted
+    const AudioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberofChannelsIOS: 2,
+      AVFormatIDIOS: AVEncodingOption.aac,
+    };
+    console.log('AudioSet', AudioSet);
+    const URI = await this.audioRecorderPlayer.startRecorder(path, AudioSet)
+    this.audioRecorderPlayer.addRecordBackListener((e) => {
+      this.setState({
+        recordSecs: e.current_position,
+        recordTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position),
+        ),
+      });
+    });
+    console.log(`uri: ${URI}`);
+  };
+
+  stop_recording = async() => {
+    const result  = await this.audioRecorderPlayer.stopRecorder();
+    
+
+    const file = {
+      uri: result,
+      name: result,
+      type: `audio/aac`
+    };
+
+    const options = {
+      keyPrefix: 'xxxxxxxx',
+      bucket: 'xxxxxxxxxx',
+      region: 'xxxxxxxxxxx',
+      accessKey: 'xxxxxxxxxxxxxxxx',
+      secretKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    };
+
+    RNS3.put(file, options)
+      .progress(event => {
+        console.log(`percent: ${event.percent}`);
+      })
+      .then(response => {
+        console.log(response, "response from rns3 audio");
+        if (response.status !== 201) {
+          console.error(response.body);
+          return;
+        }
+      })
+
+    this.audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      recordSecs: 0,
+    });
+
+    
+    console.log(result);
+  };
+
+  playback_recording = async(e) => {
+    console.log('playback_recording');
+    /*
+    const path = Platform.select({
+      ios: 'first_time.wav',
+      android: 'sdcard/first_time.wav', // should give extra dir name in android. Won't grant permission to the first level of dir.
+    });
+    */
+    const path = 'Chris.m4a';
+    const msg = await this.audioRecorderPlayer.startPlayer(path);
+    this.audioRecorderPlayer.setVolume(1.0);
+    console.log(msg);
+    this.audioRecorderPlayer.addPlayBackListener((e) => {
+      if (e.current_position === e.duration) {
+        console.log('finished');
+        this.audioRecorderPlayer.stopPlayer();
+      }
+      this.setState({
+        currentPostiionSec: e.current_position,
+        currentDurationSec: e.duration,
+        playTime: this. audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position),
+        ),
+        duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+      });
+    });
+  };
+
+  stop_playback_recording = async(e) => {
+    console.log('playback_recording');
+    this.audioRecorderPlayer.stopPlayer();
+    this.audioRecorderPlayer.removePlayBackListener();
+  };
+   //DOWNLOAD COEFF.json
+
+  render() {
+    return (
+      <Card style = {{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center'}}>
+        <Text style = {{flex: .22, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center', fontSize: 20}}>
+          Please record the displayed sentence
+        </Text>
+        <Text style = {{flex: .2, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center', fontSize: 30}}>
+          The sun is shining bright today
+        </Text>
+        <Title style = {{Title: 'centered'}}>
+          {this.state.recordTime}
+        </Title>
+        <Button mode = "contained" onPress = {() => this.start_recording()}>
+          RECORD
+        </Button>
+        <Button mode = "outlined"  onPress = {() => this.stop_recording()}>
+          STOP
+        </Button>
+        < Divider/>
+        <Title>
+          {this.state.playTime} / {this.state.duration}
+        </Title>
+        <Button mode = "contained"  onPress = {() => this.playback_recording()}>
+          PLAY
+        </Button>
+        <Button mode = "outlined"  onPress = {() => this.stop_playback_recording()}>
+          STOP
+        </Button>
+        <Text style = {{textAlignVertical: "bottom", textAlign: "center", fontSize: 30, color: "black"}}>
+          Match:
+        </Text>
+        < Divider/>
+        <Text style = {{textAlignVertical: "bottom", textAlign: "center", fontSize: 30, color: "black"}}>
+          The speaker is Chris
+        </Text>
+        <View style = {{marginBottom: 100}}>
+        </View>
+      </Card>
+    )
+  }
+}
+
 
 /*
 <Button style = {{height: 80, backgroundColor: 'green'}} onPress = {() => this.create_file()}>

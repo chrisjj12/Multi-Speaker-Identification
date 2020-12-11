f3cabb6d65fea16b5e9768e6354008d5bf035324
Christopher Jung <cjung@bu.edu>
2020-12-10 23:27:20 -0500

    changes

--- MSI/DSP/test.py
+++ MSI/DSP/test.py
@@ -2,6 +2,5 @@ import scipy.io.wavfile as wav
 import os
 from pydub import AudioSegment
 
-sound = AudioSegment.from_file("hello.m4a", format = "m4a")
-wavfile = sound.export("test.wav", format="wav")
-print(wavfile.name)
+sound = AudioSegment.from_file("Chris.m4a", format = "m4a")
+sound.export("Chris.wav", format="wav")

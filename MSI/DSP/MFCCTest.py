7e4513a2e2b0ee71ea6b5dd7129547024acfbcfc
Christopher Jung <cjung@bu.edu>
2020-12-11 05:36:57 -0500

    changes

--- MSI/DSP/MFCCTest.py
+++ MSI/DSP/MFCCTest.py
@@ -19,14 +19,13 @@ app = Flask(__name__, template_folder = "Templates")
 FlaskJSON(app)
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def create_json():
 
 
     s3_client = boto3.client("s3")
     #s3audio = s3_client("https://iostoflask.s3.us-east-2.amazonaws.com/audio/file%3A///Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/0EFB813A-C67D-45E1-9826-7A5EBF0AD6BC/Library/Caches/Chris.m4a")
     #s3audio = s3_client.download_file('iostoflask', 'audio/file:///Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/0EFB813A-C67D-45E1-9826-7A5EBF0AD6BC/Library/Caches/Chris.m4a', 'downloaded.m4a')
-    s3audio = s3_client.download_file("iostoflask", "audio/file:///Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/0EFB813A-C67D-45E1-9826-7A5EBF0AD6BC/Library/Caches/Chris.m4a", "downloaded.m4a")
 
     print('sfgasg')
 

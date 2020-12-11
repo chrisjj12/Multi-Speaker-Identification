621eadffbb998c47b94d6832cf3b87e9898a6696
Christopher Jung <cjung@bu.edu>
2020-12-10 21:28:02 -0500

    creating .json file

--- MSI/DSP/MFCCTest.py
+++ MSI/DSP/MFCCTest.py
@@ -8,48 +8,24 @@ import json
 import scipy.io.wavfile as wav
 import os
 from pydub import AudioSegment
+import boto3
 
 #UPLOAD_FOLDER = '/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'
 #ALLOWED_EXTENSIONS = {'m4a', 'wav'}
 
 app = Flask(__name__, template_folder = "Templates")
 FlaskJSON(app)
-#app.config['/Users/chrisjung/Library/Developer/CoreSimulator/Devices/5ED1D61C-0B4C-4117-BC61-79D31733A199/data/Containers/Data/Application/7E003075-2255-486D-B6E8-A1DF3D8365D5/Library/Caches'] = UPLOAD_FOLDER
-"""
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-"""
 
-
-@app.route('/audio_record', methods=['POST'])
-def save_record():
-
-    logger.info("welcome to upload`")
-    # file = request.files['file']
-    #filename = secure_filename(file.title)
-
-    file = request.files['file']
-
-    print('File from the POST request is: {}'.format(file))
-    try: 
-        read_audio_file(file[0])
-        return "****** Audio Read ******"
-    except: 
-        print("In the except", file[0]) # Gets printed as undefined
-        title = request.form['title']
-        print(title) # Able to print title 
-        return "Request received and responded"
-        # app.logger.debug(request.files['file'].filename) 
 
 @app.route('/')
 def create_json():
     
     
+    #s3 = boto3.client('s3')
+    #s3audio = s3.download_file('iostoflask', '/audio', 'hello.m4a')
 
-
-            # convert wav to mp3                                                            
-    #sound = AudioSegment.from_file("hello.m4a", format = "m4a")
+    # convert wav to mp3                                                            
+    #sound = AudioSegment.from_file(s3audio, format = "m4a")
     #wavfile = sound.export("test.wav", format="wav")
     #file_name = wavefile.name
    
@@ -61,6 +37,9 @@ def create_json():
     python_arr = fbank_feat[1:3,:]
     json_conv = python_arr.tolist()
     database_format = json.dumps({"Chris": json_conv}) # Need to change to the user inputed name in the application
+    with open('coeff.json', 'w') as json_file:
+        json_file.write(database_format)
+
 
 
     return render_template('main.html', dblist =  database_format)
